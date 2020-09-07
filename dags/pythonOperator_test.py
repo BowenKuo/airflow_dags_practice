8e910bd74e26ed001f6b3a5df693b29687ec1407
Bowen Kuo <bowen@BowenBonio.local>
2020-09-07 15:14:01 +0800

    add python executor example

---
+++ dags/pythonOperator_test.py
@@ -0,0 +1,31 @@
+import time
+from pprint import pprint
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator, PythonVirtualenvOperator
+from airflow.utils.dates import days_ago
+
+args = {
+    'owner': 'airflow',
+}
+
+dag = DAG(
+    dag_id='example_python_operator',
+    default_args=args,
+    schedule_interval=None,
+    start_date=days_ago(2),
+    tags=['example']
+)
+
+def print_context(ds, **kwargs):
+    pprint(kwargs)
+    print(ds)
+    return 'Whatever you return gets printed in the logs'
+
+
+run_this = PythonOperator(
+    task_id='print_the_context',
+    provide_context=True,
+    python_callable=print_context,
+    dag=dag,
+)
