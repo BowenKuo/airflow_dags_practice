27131926552b344d3ba7b6534d5f575e09ab0b91
bowenkuo <thisisobowenkuo@gmail.com>
2020-09-29 15:50:43 +0800

    Update

--- dags/dump_ga_to_bq.py
+++ dags/dump_ga_to_bq.py
@@ -32,13 +32,14 @@ executalbe_r_script_name = "dump_ga_to_bq.R"
 executalbe_r_script_path = git_root_path + "/" + executalbe_r_script_name
 
 volume_mount = VolumeMount('git-root-path',
-                            mount_path='/tmp',
+                            mount_path=git_root_path,
                             sub_path=None,
                             read_only=False)
 volume_config = {
     'hostPath':
     {
-        'path': git_root_path
+        'path': git_root_path,
+        "type": "Directory"
     }
 }
 volume = Volume(name='git-root-path', configs=volume_config)
