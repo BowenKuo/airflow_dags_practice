3b61f0f20bbe430bec6d0776a6c0f969007891c6
Bowen Kuo <bowen@BowenBonio.local>
2020-09-29 11:35:49 +0800

    Remove volume

--- dags/dump_ga_to_bq.py
+++ dags/dump_ga_to_bq.py
@@ -27,11 +27,6 @@ dag = DAG(
 service_account_secret_file = Secret('volume', '/etc/ga_service_account', 'ga-system-account-json', 'bonio-da-958b900cd287.json')
 client_secret_secret_file = Secret('volume', '/etc/ga_client_secret', 'ga-client-secret-json', 'client_secret_953933740389-gdq7ift55a027a26vjvv37j5mkfcvue3.apps.googleusercontent.com.json')
 
-volume_mount = VolumeMount('test-volume',
-                            mount_path='/root/mount_file',
-                            sub_path=None,
-                            read_only=True)
-
 git_root_path = os.environ['AIRFLOW__KUBERNETES__GIT_DAGS_FOLDER_MOUNT_POINT']
 executalbe_r_script_path = "./dump_ga_to_bq.R"
 
@@ -41,7 +36,6 @@ k = KubernetesPodOperator(namespace='default',
                           arguments=[],
                           labels={"script_type": "R"},
                           secrets=[service_account_secret_file, client_secret_secret_file],
-                          volume_mounts=[volume_mount],
                           name="dump-ga-to-bq",
                           task_id="dumpping",
                           is_delete_operator_pod=True,
