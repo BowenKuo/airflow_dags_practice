3220462dfca829030298bfc3a99ca73f341baf4d
bowenkuo <thisisobowenkuo@gmail.com>
2020-10-06 16:39:56 +0800

    Update

--- dags/dump_ga_to_bq.py
+++ dags/dump_ga_to_bq.py
@@ -10,7 +10,6 @@ from datetime import datetime, timedelta
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2020,9,6),
     'email': 'bowen.kuo@bonio.com.tw',
     'email_on_failure': True,
     'email_on_retry': False,
@@ -22,6 +21,8 @@ dag = DAG(
     'dump_GA_to_BQ_DAG',
     default_args = default_args,
     schedule_interval = '@daily',
+    max_active_runs = 1,
+    start_date = datetime(2020,9,6),
     catchup = True)
 
 service_account_secret_file = Secret('volume', '/etc/ga_service_account', 'ga-system-account-json', 'ga-system-account.json')
