409de8993995477f511d961a0b82146957ae1dc7
bowenkuo <thisisobowenkuo@gmail.com>
2020-10-12 17:56:58 +0800

    Update: uncomment everything

--- dags/tutorial.py
+++ dags/tutorial.py
@@ -70,42 +70,42 @@ get_user_ids_task = KubernetesPodOperator(namespace='default',
                           dag=main_dag
                           )
 
-# SUBDAG_TASK_ID = "session_activity_dag"
-# from airflow.operators.subdag_operator import SubDagOperator
-# def get_user_session_activity(dag_id, start_date, end_date, uids):
-#     sub_dag = DAG(
-#         dag_id=dag_id)
-#     for uid in uids[0:9]:
-#         user_session_activity = KubernetesPodOperator(namespace='default',
-#                                   image="bowenkuo/dump-ga-to-bq:1.0.1",
-#                                   cmds=["Rscript"],
-#                                   arguments=["--vanilla",
-#                                              retrieve_user_session_activity_r_script_whole_path,
-#                                              start_date,
-#                                              end_date,
-#                                              uid],
-#                                   secrets=[service_account_secret_file, client_secret_secret_file],
-#                                   name="main",
-#                                   task_id="get_user_session_activity-%s" % uid,
-#                                   volumes=[volume],
-#                                   volume_mounts=[volume_mount],
-#                                   is_delete_operator_pod=False,
-#                                   get_logs=True,
-#                                   do_xcom_push=True,
-#                                   dag=sub_dag
-#                                   )
-#     return sub_dag
-#
-# uids = "{{ ti.xcom_pull(task_ids='get_user_ids_task', dag_id='ba_dag', key='retrun_value') }}"
-# print(uids)
+SUBDAG_TASK_ID = "session_activity_dag"
+from airflow.operators.subdag_operator import SubDagOperator
+def get_user_session_activity(dag_id, start_date, end_date, uids):
+    sub_dag = DAG(
+        dag_id=dag_id)
+    for uid in uids[0:9]:
+        user_session_activity = KubernetesPodOperator(namespace='default',
+                                  image="bowenkuo/dump-ga-to-bq:1.0.1",
+                                  cmds=["Rscript"],
+                                  arguments=["--vanilla",
+                                             retrieve_user_session_activity_r_script_whole_path,
+                                             start_date,
+                                             end_date,
+                                             uid],
+                                  secrets=[service_account_secret_file, client_secret_secret_file],
+                                  name="main",
+                                  task_id="get_user_session_activity-%s" % uid,
+                                  volumes=[volume],
+                                  volume_mounts=[volume_mount],
+                                  is_delete_operator_pod=False,
+                                  get_logs=True,
+                                  do_xcom_push=True,
+                                  dag=sub_dag
+                                  )
+    return sub_dag
 
-# user_session_activity = SubDagOperator(
-#     task_id=SUBDAG_TASK_ID,
-#     subdag=get_user_session_activity('%s.%s' % (MAIN_DAG_ID, SUBDAG_TASK_ID),
-#                start_date = start_date,
-#                end_date = end_date,
-#                uids = uids),
-#     dag=main_dag)
-#
-#
-# get_user_ids_task >> user_session_activity
+uids = "{{ ti.xcom_pull(task_ids='get_user_ids_task', dag_id='ba_dag', key='retrun_value') }}"
+print(uids)
+
+user_session_activity = SubDagOperator(
+    task_id=SUBDAG_TASK_ID,
+    subdag=get_user_session_activity('%s.%s' % (MAIN_DAG_ID, SUBDAG_TASK_ID),
+               start_date = start_date,
+               end_date = end_date,
+               uids = uids),
+    dag=main_dag)
+
+
+get_user_ids_task >> user_session_activity
