afbc16c0c19d6781c3730442b4c91eca8820472b
bowenkuo <thisisobowenkuo@gmail.com>
2020-10-13 16:56:17 +0800

    Update: beautify code

--- dags/tutorial.py
+++ dags/tutorial.py
@@ -1,3 +1,4 @@
+from airflow.operators.subdag_operator import SubDagOperator
 import os
 from airflow import DAG
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
@@ -10,8 +11,8 @@ from datetime import datetime, timedelta
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2020,1,1),
-    'end_date': datetime(2020,9,30),
+    'start_date': datetime(2020, 1, 1),
+    'end_date': datetime(2020, 9, 30),
     'email': 'bowen.kuo@bonio.com.tw',
     'email_on_failure': True,
     'email_on_retry': False,
@@ -22,24 +23,28 @@ default_args = {
 MAIN_DAG_ID = "ba_dag"
 main_dag = DAG(
     MAIN_DAG_ID,
-    default_args = default_args,
-    schedule_interval = '@weekly',
-    catchup = True,
-    max_active_runs = 1)
+    default_args=default_args,
+    schedule_interval='@weekly',
+    catchup=True,
+    max_active_runs=1)
 
-service_account_secret_file = Secret('volume', '/etc/ga_service_account', 'ga-service-account-json', 'ga-service-account.json')
-client_secret_secret_file = Secret('volume', '/etc/ga_client_secret', 'ga-client-secret-json', 'ga-client-secret.json')
+service_account_secret_file = Secret(
+    'volume', '/etc/ga_service_account', 'ga-service-account-json', 'ga-service-account.json')
+client_secret_secret_file = Secret(
+    'volume', '/etc/ga_client_secret', 'ga-client-secret-json', 'ga-client-secret.json')
 
 script_root_path = '/tmp/scripts'
 get_user_ids_r_script_path = "Services/ELT/DA/getUserIDs.R"
-get_user_ids_r_script_whole_path = script_root_path + "/" + get_user_ids_r_script_path
+get_user_ids_r_script_whole_path = script_root_path + \
+    "/" + get_user_ids_r_script_path
 retrieve_user_session_activity_r_script_path = "Services/ELT/DA/getUserIDs.R"
-retrieve_user_session_activity_r_script_whole_path = script_root_path + "/" + retrieve_user_session_activity_r_script_path
+retrieve_user_session_activity_r_script_whole_path = script_root_path + \
+    "/" + retrieve_user_session_activity_r_script_path
 
 volume_mount = VolumeMount('git-root-path',
-                            mount_path=script_root_path,
-                            sub_path=None,
-                            read_only=False)
+                           mount_path=script_root_path,
+                           sub_path=None,
+                           read_only=False)
 volume_config = {
     "hostPath":
     {
@@ -53,25 +58,27 @@ start_date = "{{ ds }}"
 end_date = "{{ macros.ds_add(next_ds, -1) }}"
 
 get_user_ids_task = KubernetesPodOperator(namespace='default',
-                          image="bowenkuo/dump-ga-to-bq:1.0.1",
-                          cmds=["Rscript"],
-                          arguments=["--vanilla",
-                                     get_user_ids_r_script_whole_path,
-                                     start_date,
-                                     end_date],
-                          secrets=[service_account_secret_file, client_secret_secret_file],
-                          name="main",
-                          task_id="get_user_ids",
-                          volumes=[volume],
-                          volume_mounts=[volume_mount],
-                          is_delete_operator_pod=False,
-                          get_logs=True,
-                          do_xcom_push=True,
-                          dag=main_dag
-                          )
+                                          image="bowenkuo/dump-ga-to-bq:1.0.1",
+                                          cmds=["Rscript"],
+                                          arguments=["--vanilla",
+                                                     get_user_ids_r_script_whole_path,
+                                                     start_date,
+                                                     end_date],
+                                          secrets=[
+                                              service_account_secret_file, client_secret_secret_file],
+                                          name="main",
+                                          task_id="get_user_ids",
+                                          volumes=[volume],
+                                          volume_mounts=[volume_mount],
+                                          is_delete_operator_pod=False,
+                                          get_logs=True,
+                                          do_xcom_push=True,
+                                          dag=main_dag
+                                          )
 
 SUBDAG_TASK_ID = "session_activity_dag"
-from airflow.operators.subdag_operator import SubDagOperator
+
+
 def get_user_session_activity(dag_id, start_date, end_date, uids):
     # uids = xcom_pull(task_ids='get_user_ids_task', dag_id='ba_dag', key='retrun_value')
     # , key='retrun_value'
@@ -79,40 +86,43 @@ def get_user_session_activity(dag_id, start_date, end_date, uids):
     # uids = context['task_instance'].xcom_pull(task_ids='get_user_ids_task', dag_id='ba_dag')
     sub_dag = DAG(
         dag_id=dag_id,
-        start_date = datetime.now(),
-        schedule_interval = None)
+        start_date=datetime.now(),
+        schedule_interval=None)
     print(uids)
     for uid in uids[0:9]:
         user_session_activity = KubernetesPodOperator(namespace='default',
-                                  image="bowenkuo/dump-ga-to-bq:1.0.1",
-                                  cmds=["Rscript"],
-                                  arguments=["--vanilla",
-                                             retrieve_user_session_activity_r_script_whole_path,
-                                             start_date,
-                                             end_date,
-                                             uid],
-                                  secrets=[service_account_secret_file, client_secret_secret_file],
-                                  name="main",
-                                  task_id="get_user_session_activity_%s" % uid,
-                                  volumes=[volume],
-                                  volume_mounts=[volume_mount],
-                                  is_delete_operator_pod=False,
-                                  get_logs=True,
-                                  do_xcom_push=False,
-                                  dag=sub_dag
-                                  )
+                                                      image="bowenkuo/dump-ga-to-bq:1.0.1",
+                                                      cmds=["Rscript"],
+                                                      arguments=["--vanilla",
+                                                                 retrieve_user_session_activity_r_script_whole_path,
+                                                                 start_date,
+                                                                 end_date,
+                                                                 uid],
+                                                      secrets=[
+                                                          service_account_secret_file, client_secret_secret_file],
+                                                      name="main",
+                                                      task_id="get_user_session_activity_%s" % uid,
+                                                      volumes=[volume],
+                                                      volume_mounts=[
+                                                          volume_mount],
+                                                      is_delete_operator_pod=False,
+                                                      get_logs=True,
+                                                      do_xcom_push=False,
+                                                      dag=sub_dag
+                                                      )
     return sub_dag
 
 # uids = task_instance.xcom_pull(task_ids='get_user_ids_task', dag_id='ba_dag', key='retrun_value')
 # uids = "{{ ti.xcom_pull(task_ids='get_user_ids_task', dag_id='ba_dag', key='retrun_value') }}"
 # print(uids)
 
+
 user_session_activity = SubDagOperator(
     task_id=SUBDAG_TASK_ID,
     subdag=get_user_session_activity('%s.%s' % (MAIN_DAG_ID, SUBDAG_TASK_ID),
-               start_date = start_date,
-               end_date = end_date,
-               uids = "{{ ti.xcom_pull(task_ids='get_user_ids', dag_id='ba_dag') }}"),
+                                     start_date=start_date,
+                                     end_date=end_date,
+                                     uids="{{ ti.xcom_pull(task_ids='get_user_ids', dag_id='ba_dag') }}"),
     dag=main_dag)
 
 
